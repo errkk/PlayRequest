656b03555454e8df7383e7b8bde0be5c224a4db7
Eric George <eric@madebymany.co.uk>
2019-09-01 17:10:02 +0100

    get refresh token if necessary

--- apps/lv/lib/lv/apis/macros/endpoint_helper.ex
+++ apps/lv/lib/lv/apis/macros/endpoint_helper.ex
@@ -6,52 +6,81 @@ defmodule E.Apis.EndpointHelper do
 
       @spec get(String.t()) :: any() | nil
       def get(resource) do
-        client()
-        |> authenticated_client()
-        |> Client.get(resource)
-        |> handle_api_response()
+        case client()
+          |> authenticated_client()
+          |> Client.get(resource)
+          |> handle_api_response() do
+            {:unauthorized} ->
+              get_refresh_token()
+              get(resource)
+            res -> res
+          end
       end
 
       @spec post(String.t(), map()) :: any() | nil
       def post(%{} = params, resource) do
         params = Jason.encode!(params)
-        client()
-        |> authenticated_client()
-        |> Client.post(resource, params)
-        |> handle_api_response()
+        case client()
+          |> authenticated_client()
+          |> Client.post(resource, params)
+          |> handle_api_response() do
+            {:unauthorized} ->
+              get_refresh_token()
+              post(params, resource)
+            res -> res
+          end
       end
 
       @spec post(String.t()) :: any() | nil
       def post(resource) do
-        client()
-        |> authenticated_client()
-        |> Client.post(resource)
-        |> handle_api_response()
+        case client()
+          |> authenticated_client()
+          |> Client.post(resource)
+          |> handle_api_response()
+            {:unauthorized} ->
+              get_refresh_token()
+              post(resource)
+            res -> res
+          end
       end
 
       @spec put(String.t(), map()) :: any() | nil
       def put(%{} = params, resource) do
         params = Jason.encode!(params)
-        client()
+        case client()
         |> authenticated_client()
         |> Client.put(resource, params)
-        |> handle_api_response()
+        |> handle_api_response() do
+            {:unauthorized} ->
+              get_refresh_token()
+              put(params, resource)
+            res -> res
+          end
       end
 
       @spec delete(String.t()) :: any() | nil
       def delete(resource) do
-        client()
-        |> authenticated_client()
-        |> Client.delete(resource)
-        |> handle_api_response()
+        case client()
+          |> authenticated_client()
+          |> Client.delete(resource)
+          |> handle_api_response() do
+            {:unauthorized} ->
+              get_refresh_token()
+              delete(resource)
+            res -> res
+          end
       end
 
+      defp handle_api_response({:error, %Response{status_code: 401, body: body}}) do
+        Logger.error("Unauthorized token")
+        {:unauthorized}
+      end
       defp handle_api_response({:ok, %Response{status_code: 200, body: body}}), do: Jason.decode!(body) |> convert_result()
       defp handle_api_response({:ok, %Response{status_code: 204, body: body}}), do: {:ok, nil}
       defp handle_api_response({:error, %Response{status_code: 404, body: body}}), do: Logger.error("Not found")
       defp handle_api_response({:error, %Response{status_code: status_code, body: body}}), do: Logger.error("error: #{status_code}")
-      defp handle_api_response({:error, %Response{status_code: 401, body: body}}), do: Logger.error("Unauthorized token")
       defp handle_api_response({:error, %Error{reason: reason}}), do: Logger.error("Error: #{inspect reason}")
+
 
       @spec authenticated_client(Client.t()) :: Client.t() | {:error, atom()}
       defp authenticated_client(client) do
