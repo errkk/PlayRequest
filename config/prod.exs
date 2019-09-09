3eb03ce17183c8e84aa82c6c185d68a3a15cdc66
Eric George <eric@madebymany.co.uk>
2019-09-09 22:05:50 +0100

    endpoint server

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :pr_web, PRWeb.Endpoint,
+  server: true,
   url: [scheme: "https", host: "playrequest.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
