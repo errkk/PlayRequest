bef954672368ed846340091c110d8050a19fca88
Eric George <eric@madebymany.co.uk>
2019-09-09 21:46:16 +0100

    release config;

---
+++ config/releases.exs
@@ -0,0 +1,7 @@
+import Config
+
+config :pr, PR.Repo,
+  ssl: true,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
--- rel/envvars.exs
+++ rel/envvars.exs
@@ -8,8 +8,3 @@ config :pr, :spotify,
   user_id: System.get_env("SPOTIFY_USER_ID"),
   key: System.get_env("SPOTIFY_CLIENT_ID"),
   secret: System.get_env("SPOTIFY_SECRET")
-
-config :pr, PR.Repo,
-  ssl: true,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
