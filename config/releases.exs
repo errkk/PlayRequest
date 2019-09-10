c26a1b39a6020af2bbd7e19681cbd9aab680b44a
Eric George <eric@madebymany.co.uk>
2019-09-10 19:24:08 +0100

    bind http

--- config/releases.exs
+++ config/releases.exs
@@ -7,8 +7,7 @@ config :pr, PR.Repo,
 
 config :pr_web, PRWeb.Endpoint,
   server: true,
-  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: String.to_integer(System.get_env("PORT"))],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "0.0.0.0", port: String.to_integer(System.get_env("PORT"))],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 import_config "../../envvars.exs"
