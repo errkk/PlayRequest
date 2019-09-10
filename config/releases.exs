df90efde3e5eb9a8cf5119566089330438616c75
Eric George <eric@madebymany.co.uk>
2019-09-10 19:04:55 +0100

    release tasks and trying port var

--- config/releases.exs
+++ config/releases.exs
@@ -5,4 +5,10 @@ config :pr, PR.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
+config :pr_web, PRWeb.Endpoint,
+  server: true,
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: String.to_integer(System.get_env("PORT"))],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
 import_config "../../envvars.exs"
