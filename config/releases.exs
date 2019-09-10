beac1f0d2c41322037694e75278178a74e8786fe
Eric George <eric@madebymany.co.uk>
2019-09-10 21:01:08 +0100

    ok what about this fffs

--- config/releases.exs
+++ config/releases.exs
@@ -5,9 +5,12 @@ config :pr, PR.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-config :pr_web, PR.Endpoint,
+config :pr_web, PRWeb.Endpoint,
   server: true,
-  http: [port: String.to_integer(System.get_env("PORT")],
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :logger, level: :info
 
 import_config "../../envvars.exs"
