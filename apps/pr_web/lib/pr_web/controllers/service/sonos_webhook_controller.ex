7f826047dfd2075cfbd856fc49b6a211df513035
Eric George <eric@madebymany.co.uk>
2019-09-13 09:59:00 +0100

    log webhooks

--- apps/pr_web/lib/pr_web/controllers/service/sonos_webhook_controller.ex
+++ apps/pr_web/lib/pr_web/controllers/service/sonos_webhook_controller.ex
@@ -6,12 +6,14 @@ defmodule PRWeb.Service.SonosWebhookController do
   alias PR.Music.SonosItem
 
   def callback(conn, %{"playbackState" => _} = params) do
+    Logger.info("Playback state webhook")
     params
     |> PlayState.handle_play_state_webhook()
     render(conn, "index.json")
   end
 
   def callback(conn, %{"currentItem" => _} = params) do
+    Logger.info("Metadata webhook")
     params
     |> PlayState.handle_metadata_webhook()
 
