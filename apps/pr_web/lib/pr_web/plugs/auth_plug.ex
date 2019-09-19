e33615559af980a829158539dcdeb57dc0766409
Eric George <eric@madebymany.co.uk>
2019-09-19 09:37:31 +0100

    redirect

--- apps/pr_web/lib/pr_web/plugs/auth_plug.ex
+++ apps/pr_web/lib/pr_web/plugs/auth_plug.ex
@@ -9,15 +9,28 @@ defmodule PRWeb.Plug.AuthPlug do
   def init(opts), do: opts
 
   def call(conn, _) do
-    user_id = get_session(conn, :user_id)
+    conn
+    |> get_session(:user_id)
+    |> get_user(conn)
+  end
+
+  defp get_user(nil, conn) do
+    nope(conn)
+  end
+
+  defp get_user(user_id, conn) do
     case Auth.get_user(user_id) do
         %User{} = user ->
           conn
           |> put_session(:user_id, user_id)
           |> assign(:current_user, user)
         _ ->
-          conn
-          |> redirect(to: Routes.auth_path(conn, :index))
+        nope(conn)
       end
+  end
+
+  defp nope(conn) do
+    conn
+    |> redirect(to: Routes.auth_path(conn, :index))
   end
 end
