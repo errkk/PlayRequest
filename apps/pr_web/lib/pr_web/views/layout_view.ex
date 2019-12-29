f9a9e6da0159795410acc4bdbadbefe38f8f9a81
Eric George <eric@madebymany.co.uk>
2019-12-29 13:59:02 +0000

    auto stop the snow

--- apps/pr_web/lib/pr_web/views/layout_view.ex
+++ apps/pr_web/lib/pr_web/views/layout_view.ex
@@ -1,3 +1,16 @@
 defmodule PRWeb.LayoutView do
   use PRWeb, :view
+
+  def is_snow_season? do
+    {:ok, from} =
+      Date.utc_today()
+      |> Map.get(:year)
+      |> Date.new(12, 1)
+
+    to =
+      from
+      |> Map.put(:day, 26)
+
+    Date.utc_today |> Timex.between?(from, to)
+  end
 end
