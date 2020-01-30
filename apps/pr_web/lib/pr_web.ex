eab318c06949585547f921c7a0235282f2c10fb3
Eric George <eric@madebymany.co.uk>
2020-01-30 17:31:12 +0000

    new session arrangement for liveview > 0.5.0

--- apps/pr_web/lib/pr_web.ex
+++ apps/pr_web/lib/pr_web.ex
@@ -23,7 +23,7 @@ defmodule PRWeb do
       import Plug.Conn
       import PRWeb.Gettext
       alias PRWeb.Router.Helpers, as: Routes
-      import Phoenix.LiveView.Controller, only: [live_render: 3]
+      import Phoenix.LiveView.Helpers
     end
   end
 
@@ -35,7 +35,7 @@ defmodule PRWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
-      import Phoenix.LiveView, only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2]
+      import Phoenix.LiveView.Helpers
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
