3095504e0a80e89660d3696c78e28fa4e8347145
Eric George <eric@madebymany.co.uk>
2020-02-20 16:24:01 +0000

    controlled

--- apps/pr_web/assets/js/favicon.js
+++ apps/pr_web/assets/js/favicon.js
@@ -2,6 +2,7 @@ export const ICON_SIZE = 32;
 
 export default () => {
   const favicon = document.getElementById("favicon");
+  const playState = document.getElementById("play_state");
   const worker = new Worker('/js/worker.js');
   const offcanvas = new OffscreenCanvas(ICON_SIZE, ICON_SIZE);
   worker.postMessage(offcanvas, [offcanvas]);
@@ -12,6 +13,9 @@ export default () => {
   const ctx = canvas.getContext('2d');
 
   worker.onmessage = ({data}) => {
+    if (!playState || playState.dataset.playback !== "active") {
+      return;
+    }
     ctx.clearRect(0, 0, ICON_SIZE, ICON_SIZE);
     ctx.drawImage(data, 0, 0);
     favicon.href = canvas.toDataURL();
