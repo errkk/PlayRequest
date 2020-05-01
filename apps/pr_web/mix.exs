deda1eaf8b8c6dd5a1dcbebfdc3e70a3c1468274
Eric George <eric@madebymany.co.uk>
2020-05-01 13:54:30 +0100

    :os_mon for live_dashboard

--- apps/pr_web/mix.exs
+++ apps/pr_web/mix.exs
@@ -24,7 +24,7 @@ defmodule PRWeb.MixProject do
   def application do
     [
       mod: {PRWeb.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
