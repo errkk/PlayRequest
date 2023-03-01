155e44bc1e308ba9f39bd0dff013186204801c7b
Eric George <errkk@hey.com>
2023-03-01 09:54:12 +0000

    cookie

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,8 @@ bin/pr eval "PR.Release.migrate";
 
 ip=$(grep fly-local-6pn /etc/hosts | cut -f 1)
 export RELEASE_DISTRIBUTION=name
+# This has to be the same between all nodes that cluster
+export RELEASE_COOKIE=$FLY_APP_NAME
 export RELEASE_NODE=$FLY_APP_NAME@$ip
 
 echo "Starting server ${RELEASE_NODE}"
