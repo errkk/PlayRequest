b0262a94ab66bba7c55f3bf63bee70067e481832
Eric George <errkk@hey.com>
2023-03-01 09:04:09 +0000

    trying libcluster

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,11 @@ bin/pr eval "PR.Release.migration_status";
 echo "Running migrations"
 bin/pr eval "PR.Release.migrate";
 
-echo "Starting server"
+ip=$(grep fly-local-6pn /etc/hosts | cut -f 1)
+export RELEASE_DISTRIBUTION=name
+export RELEASE_NODE=$FLY_APP_NAME@$ip
+
+echo "Starting server ${RELEASE_NODE}"
 bin/pr start;
 
 
