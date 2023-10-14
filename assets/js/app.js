a8353644c5aacc88e16768fe556239708f194e8a
Eric George <errkk@hey.com>
2023-10-14 15:41:14 +0100

    do ghost if its halloween week

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,18 @@ import "phoenix_html"
 import { LiveSocket } from "phoenix_live_view"
 import { Socket } from "phoenix"
 
+import initGhost from "./ghost"
 import notifications from "./notifications";
 
 const csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 const liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } });
 liveSocket.connect();
+
+// Load the ghost up after live view connects
+const socket = liveSocket.getSocket()
+socket.onOpen(() => {
+  setTimeout(initGhost, 1000)
+})
 
 const searchInput = document.getElementById("search");
 
