cc80d22fb8b48d2cd478019c51db40d497aecc20
Eric George <errkk@hey.com>
2023-10-30 09:03:18 +0000

    no day check

--- lib/pr_web/components/layouts.ex
+++ lib/pr_web/components/layouts.ex
@@ -4,17 +4,8 @@ defmodule PRWeb.Layouts do
   embed_templates("layouts/*")
 
   def ghost(assigns) do
-    halloween = ~D[2023-10-31]
-
-    if Timex.today()
-       |> Timex.between?(Timex.beginning_of_week(halloween), Timex.end_of_week(halloween)) do
       ~H"""
-      <.ghost_shader />
+        <.ghost_shader />
       """
-    else
-      ~H"""
-
-      """
-    end
   end
 end
