d632efd8f364b482fc53471557371749534b5819
Eric George <errkk@hey.com>
2024-10-17 09:20:49 +0100

    test week

--- lib/pr_web/components/layouts.ex
+++ lib/pr_web/components/layouts.ex
@@ -4,7 +4,7 @@ defmodule PRWeb.Layouts do
   embed_templates("layouts/*")
 
   def ghost(assigns) do
-    if same_week?(Timex.today(), ~D[2023-10-31]) do
+    if same_week?(Timex.today(), ~D[2024-10-17]) do
       ~H"""
       <.ghost_shader />
       """
@@ -16,9 +16,9 @@ defmodule PRWeb.Layouts do
   end
 
   defp same_week?(date1, date2) do
-    {year1, week1, _weekday} = Timex.iso_triplet(date1)
-    {year2, week2, _weekday} = Timex.iso_triplet(date2)
+    {_year1, week1, _weekday} = Timex.iso_triplet(date1)
+    {_year2, week2, _weekday} = Timex.iso_triplet(date2)
 
-    {year1, week1} == {year2, week2}
+    week1 == week2
   end
 end
